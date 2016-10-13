258cb82a19aa39475dbdd0743397a7de6e9a6deb
djianLucca <djianlucca@outlook.com>
2016-10-13 02:42:23 -0300

    teste de  form-horizontal

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -34,26 +34,6 @@
       <!-- Formulário -->  
       <form>
 
-      <!-- Exemplo
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Email">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputFile">File input</label>
-          <input type="file" id="exampleInputFile">
-          <p class="help-block">Example block-level help text here.</p>
-        </div>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox"> Check me out
-          </label>
-        </div>
-      -->
         <div class="row">
           
           <div class="col-md-10 col-md-offset-1">
@@ -77,6 +57,7 @@
                     <option>Remessa brinde</option>
                   </select>
                 </div>
+
                 <div class="form-group col-md-4">
                   <label>Forma de pagamento</label>
                   <select class="form-control">
@@ -84,6 +65,7 @@
                     <option>A prazo</option>
                   </select>
                 </div>
+
               </div>
             </fieldset>
           </div>
@@ -100,6 +82,7 @@
                 <div class="col-md-6">
                   <fieldset>
                     <legend><h4>Identificação</h4></legend>
+
                       <div class="row">
                         <div class="form-group col-md-6">
                           <label>Tipo de operação</label>
@@ -114,17 +97,21 @@
                           <input type="text" class="form-control" placeholder="Nº Documento">
                         </div>
                       </div>
+
                       <div class="row">
                         <div class="form-group col-md-12">
                           <label>Razão Social/Nome</label>
                           <input type="text" class="form-control" placeholder="Razão Social/Nome">
                         </div>
                       </div>
+
                     </fieldset>
                 </div>
+
                 <div class="col-md-6">
                   <fieldset>
                     <legend><h4>Endereço</h4></legend>
+
                     <div class="row">
                       <div class="form-group col-md-9">
                         <label>Logradouro</label>
@@ -135,6 +122,7 @@
                         <input type="text" class="form-control" placeholder="Nº">
                       </div>
                     </div>
+
                     <div class="row">
                       <div class="form-group col-md-5">
                         <label>Bairro</label>
@@ -153,6 +141,7 @@
                           </select>
                       </div>
                     </div>
+
                     <div class="row">
                       <div class="form-group col-md-12">
                         <label>Cidade</label>
@@ -164,6 +153,7 @@
                           </select>
                       </div>
                     </div>
+
                   </fieldset>
                 </div>
               </div>
@@ -179,8 +169,10 @@
             <fieldset>
               <legend><h3><b>Dados do produto</b></h3></legend>
               <div class="row">
+
                 <div class="col-md-7">
                   <div class="row">
+
                     <div class="form-group col-md-6">
                       <label>Código</label>
                       <input type="text" class="form-control" placeholder="Código">
@@ -194,20 +186,25 @@
                     </div>
                   </div>
                   <div class="row">
+
                     <div class="form-group col-md-4">
                       <label>NCM</label>
                       <input type="text" class="form-control" placeholder="Ncm">
                     </div>
+
                     <div class="form-group col-md-4">
                       <label>Unidade comercial</label>
                       <input type="text" class="form-control" placeholder="Und">
                     </div>
+
                     <div class="form-group col-md-4">
                       <label>Quantidade comercial</label>
                       <input type="text" class="form-control" placeholder="Qtd">
                     </div>
+
                   </div>
                   <div class="row">
+
                     <div class="col-md-6">
                       <label>Valor unitario comercial</label>
                       <div class="input-group">
@@ -216,6 +213,7 @@
                         <span class="input-group-addon">.00</span>
                       </div>
                     </div>
+
                     <div class="col-md-6">
                       <label>Valor produto</label>
                       <div class="input-group">
@@ -224,6 +222,7 @@
                         <span class="input-group-addon">.00</span>
                       </div>
                     </div>
+                    
                   </div>
                 </div>
                 <div class="col-md-5">
@@ -239,6 +238,7 @@
 
         <br/>
         <div class="row">
+
           <div class="col-md-1 col-md-offset-5">
             <button type="submit" class="btn btn-primary btn-lg">Submit</button>
           </div>
